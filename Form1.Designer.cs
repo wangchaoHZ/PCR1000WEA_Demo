fe7d296f1b87d7eaa67bc72fc2e213b3b278dea1
wangchaoHZ <2399264862@qq.com>
2023-06-30 15:31:12 +0800

    init .

---
+++ Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace KiKuSuiPowerSet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("等线", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.button1.Location = new System.Drawing.Point(30, 121);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(197, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "连接设备";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("等线", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.textBox1.Location = new System.Drawing.Point(397, 144);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(127, 44);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("等线", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.Navy;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 201);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(572, 237);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("等线", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label1.Location = new System.Drawing.Point(26, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(512, 22);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "通信协议：SCPI协议  IP地址:192.168.1.127 端口:5024";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("等线", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label2.Location = new System.Drawing.Point(286, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 22);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "电压设置:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("等线", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label3.Location = new System.Drawing.Point(530, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 30);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "V";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("等线", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.button2.Location = new System.Drawing.Point(397, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(187, 33);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "点我查看如何使用";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("等线", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(286, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 22);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "输出限值:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("等线", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox2.ForeColor = System.Drawing.Color.Red;
+            this.textBox2.Location = new System.Drawing.Point(397, 92);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(127, 44);
+            this.textBox2.TabIndex = 10;
+            this.textBox2.Text = "24";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyDown);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("等线", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(530, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 30);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "V";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "智控实验室菊水电源操作工具";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace KiKuSuiPowerSet
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = string.Empty;
+        }
+
+        string serverIP = "192.168.1.127";
+        int serverPort = 5024;
+        TcpClient client;
+        NetworkStream networkStream;
+
+        string LAST_VALUE = string.Empty;
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (button1.Text.Contains("连接"))
+            {
+                button1.Text = "断开设备";
+                richTextBox1.Clear();
+                client = new TcpClient();
+                client.Connect(serverIP, serverPort);
+
+                networkStream = client.GetStream();
+                string message = "VOLT:RANG 322\n";
+                byte[] data = Encoding.ASCII.GetBytes(message);
+                // 发送消息到服务器
+                networkStream.Write(data, 0, data.Length);
+
+                byte[] buffer = new byte[256];
+                int bytesRead = networkStream.Read(buffer, 0, buffer.Length);
+                string response = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                response =  response.Replace("SCPI>", "SCPI> OK");
+                richTextBox1.Text = richTextBox1.Text + response;
+                Thread.Sleep(300);
+
+                message = "OUTP OFF\n";
+                data = Encoding.ASCII.GetBytes(message);
+                // 发送消息到服务器
+                networkStream.Write(data, 0, data.Length);
+                Thread.Sleep(300);
+
+                message = "VOLT:OFFS 0\n";
+                data = Encoding.ASCII.GetBytes(message);
+                // 发送消息到服务器
+                networkStream.Write(data, 0, data.Length);
+                //buffer = new byte[256];
+                //bytesRead = networkStream.Read(buffer, 0, buffer.Length);
+                //response = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                //richTextBox1.Text = richTextBox1.Text + response;
+                Thread.Sleep(500);
+
+                message = "OUTP ON\n";
+                data = Encoding.ASCII.GetBytes(message);
+                // 发送消息到服务器
+                networkStream.Write(data, 0, data.Length);
+
+                //buffer = new byte[256];
+                //bytesRead = networkStream.Read(buffer, 0, buffer.Length);
+                //response = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                //richTextBox1.Text = richTextBox1.Text + response;
+                richTextBox1.Text = richTextBox1.Text + "\n电源已连接\n";
+            }
+            else if (button1.Text.Contains("断开"))
+            {
+                button1.Text = "连接设备";
+                string message = "OUTP OFF\n";
+                byte[] data = Encoding.ASCII.GetBytes(message);
+                // 发送消息到服务器
+                networkStream.Write(data, 0, data.Length);
+                Thread.Sleep(300);
+                byte[] buffer = new byte[256];
+                int bytesRead = networkStream.Read(buffer, 0, buffer.Length);
+                string response = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                response = response.Replace("SCPI>", "SCPI> OK");
+                richTextBox1.Text = richTextBox1.Text + response;
+                Thread.Sleep(300);
+                client.Close();
+                richTextBox1.Text = richTextBox1.Text + "\n电源已断开\n";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "通信协议：SCPI协议  IP地址: 192.168.1.127  端口: 5024";
+            textBox2.Text = "24";
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+
+        }
+
+        float maxOut;
+        float value;
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            if (float.TryParse(textBox2.Text, out maxOut))
+            {
+                if (maxOut > 450.0)
+                {
+                    maxOut = (float)450.0;
+                }
+            }
+            else
+            {
+                MessageBox.Show("设置输出限值错误, 是不是有效数字");
+                return;
+            }
+            textBox1.Text = textBox1.Text.Replace("。", ".");
+            if (float.TryParse(textBox1.Text, out value))
+            {
+                if (button1.Text.Contains("连接"))
+                {
+                    MessageBox.Show("请先连接设备!!!");
+                    textBox1.Text = string.Empty;
+                    return;
+                }
+
+                if (value > maxOut)
+                {
+                    MessageBox.Show("输入超限制，最大输出：" + maxOut.ToString());
+                    //return;//
+                    textBox1.Text = maxOut.ToString();
+                }
+
+                if (LAST_VALUE != textBox1.Text)
+                {
+                    LAST_VALUE = textBox1.Text;
+                    richTextBox1.Text = richTextBox1.Text + "\n设置电压:" + value.ToString() + "V";
+                    string message = "VOLT:OFFS " + textBox1.Text + "\n";
+                    byte[] data = Encoding.ASCII.GetBytes(message);
+                    // 发送消息到服务器
+                    networkStream.Write(data, 0, data.Length);
+
+                    byte[] buffer = new byte[256];
+                    int bytesRead = networkStream.Read(buffer, 0, buffer.Length);
+                    string response = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                    response = response.Replace("SCPI>", "SCPI> OK");
+                    richTextBox1.Text = richTextBox1.Text + response;
+                }
+            }
+
+            richTextBox1.SelectionStart = richTextBox1.Text.Length;
+            richTextBox1.ScrollToCaret();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ((int)e.KeyCode == 13)
+            {
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ((int)e.KeyCode == 13)
+            {
+                e.SuppressKeyPress = true;
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = string.Empty;
+            string helpInfo = "1. 网线连接电源后LAN口\r\n\r\n2. 网线另一端连接电脑网口\r\n\r\n3. 设置电脑IPv4地址:192.168.1.123\r\n\r\n4. 子网掩码:255.255.255.0\r\n\r\n5. 点击连接设备\r\n\r\n6. 设置输出电压";
+            richTextBox1.Text = helpInfo;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (float.TryParse(textBox2.Text, out maxOut))
+            {
+                if (maxOut > 450.0)
+                {
+                    maxOut = (float)450.0;
+                    return;
+                }
+
+                if (value > maxOut)
+                {
+                    textBox1.Text = "0.0";
+
+                    if (button1.Text.Contains("断开"))
+                    {
+                        string message = "VOLT:OFFS 0\n";
+                        byte[] data = Encoding.ASCII.GetBytes(message);
+                        // 发送消息到服务器
+                        networkStream.Write(data, 0, data.Length);
+
+                        byte[] buffer = new byte[256];
+                        int bytesRead = networkStream.Read(buffer, 0, buffer.Length);
+                        string response = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                        response = response.Replace("SCPI>", "SCPI> OK");
+                        richTextBox1.Text = richTextBox1.Text + response;
+                    }
+                }
+            }
+        }
+    }
+}
